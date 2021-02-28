ef52922f4918b52722cf70289fdf7eea0054d898
Jim Burch <jrburch1@gmail.com>
2021-02-27 19:39:55 -0700

    added tweet feature

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,10 @@
 import { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
 import Head from 'next/head';
+import MadLibs from '../components/MadLibs';
 // import Link from 'next/link';
 
 const Index = () => {
-  const [ count, setCount ] = useState(0);
-
-  // useEffect(() => {
-  //   document.title = `${count} clicks`
-  // })
-
-  const increase = () => {
-    setCount(count + 1);
-  }
 
   return (
     <Layout>
@@ -20,15 +12,7 @@ const Index = () => {
         <title>HoopsBot</title>
       </Head>
       <h1>HoopsBot</h1>
-      <img src="../static/devinbooker.jpg" alt="Devin Armani Booker" height="250px" />
-      <button onClick={increase}>There are {count} takes</button>
-
-      <style jsx>{`
-        h1 {
-          color: purple;
-          font-size: 35px;
-        }
-      `}</style>
+      <MadLibs />
     </Layout>
   )
 };
