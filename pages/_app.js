0c07b1ec30b4699c5f6b0dbe84b9b987e3bdf084
Jim Burch <49823725+JimBurch@users.noreply.github.com>
2022-08-15 21:52:47 -0700

    Openai v2 (#2)
    * initial setup
    
    * openai api
    
    * setup openai js and generate front end response

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import App from 'next/app';
-import '../styles.css';
-
-const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
-
-export default MyApp;
---
+++ src/pages/api/openai.js
@@ -0,0 +1,41 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const { OPENAI_AUTH, OPENAI_ORG } = process.env;
+
+const configuration = new Configuration({
+	organization: OPENAI_ORG,
+	apiKey: OPENAI_AUTH,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const handler = async (req, res) => {
+	try {
+		const response = await openai.listEngines();
+		const completion = await openai.createCompletion({
+			model: 'text-davinci-002',
+			prompt: 'A controversial and outlandish NBA hot take in one sentence:',
+			n: 1,
+			max_tokens: 50,
+			temperature: 0.9,
+			top_p: 1,
+			best_of: 1,
+			stop: ['\\n'],
+		});
+
+		return res.status(200).json(completion.data.choices[0].text);
+	} catch (error) {
+		if (error.response) {
+			console.log(error.response.status);
+			console.log(error.response.data);
+		} else {
+			console.log(error.message);
+		}
+	}
+};
+
+export default handler;
+
+// https://github.com/openai/openai-node
+// https://beta.openai.com/docs/api-reference/introduction
+// https://nextjs.org/docs/api-routes/introduction
