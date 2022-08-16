e69e7f13e6c2dc5178d7396c5f5206c7c089b606
Jim Burch <jrburch1@gmail.com>
2022-08-15 21:51:12 -0700

    setup openai js and generate front end response

--- src/pages/api/openai.js
+++ src/pages/api/openai.js
@@ -14,19 +14,23 @@ const handler = async (req, res) => {
 		const response = await openai.listEngines();
 		const completion = await openai.createCompletion({
 			model: 'text-davinci-002',
-			prompt: 'Generate a controversion and outlandish NBA hot take',
-			// engine: 'davinci',
+			prompt: 'A controversial and outlandish NBA hot take in one sentence:',
+			n: 1,
 			max_tokens: 50,
 			temperature: 0.9,
-			// topP: 1,
-			stop: 'input',
+			top_p: 1,
+			best_of: 1,
+			stop: ['\\n'],
 		});
-
-		console.log(completion.data);
 
 		return res.status(200).json(completion.data.choices[0].text);
 	} catch (error) {
-		console.log(error);
+		if (error.response) {
+			console.log(error.response.status);
+			console.log(error.response.data);
+		} else {
+			console.log(error.message);
+		}
 	}
 };
 
