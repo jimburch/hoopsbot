fe9c61c863d90149b30419d265354ffc787027e6
Jim Burch <jrburch1@gmail.com>
2022-08-23 22:01:27 -0700

    setup api to save takes and seed data file

--- migrations/20220816053853_create_takes_table.js
+++ migrations/20220816053853_create_takes_table.js
@@ -3,14 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = async function (knex) {
-	return await knex.schema.createTable('takes', t => {
-		t.increments('id');
-		t.string('take');
-		t.integer('hot');
-		t.integer('cold');
-		t.integer('shares');
-		t.timestamps(true, true);
-	});
+  return await knex.schema.createTable("takes", (t) => {
+    t.increments("id");
+    t.string("take");
+    t.integer("hot");
+    t.integer("cold");
+    t.integer("shares");
+    t.timestamps(true, true);
+  });
 };
 
 /**
@@ -18,5 +18,5 @@ exports.up = async function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function (knex) {
-	return await knex.schema.dropTable('takes');
+  return await knex.schema.dropTable("takes");
 };
