17dbcbce6c84fe242f9f424ef74f8232772c1f33
Jim Burch <jrburch1@gmail.com>
2022-10-19 11:35:17 -0700

    removed db files and scripts

--- src/data/db.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const knex = require("knex");
-const knexfile = require("../../knexfile");
-
-const env = process.env.NODE_ENV || "development";
-const configOptions = knexfile[env];
-
-module.exports = knex(configOptions);
